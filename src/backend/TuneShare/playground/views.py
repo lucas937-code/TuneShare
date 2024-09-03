d7e870996157325a218ce5fd840deed24518c249
Lucas Hein <lucas.hein.dev@gmail.com>
2024-09-03 17:34:56 +0200

    updated gitignore

--- src/backend/TuneShare/playground/views.py
+++ src/backend/TuneShare/playground/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+import os
+from dotenv import load_dotenv
 
-# Create your views here.
+load_dotenv()
+
+url: str = os.environ.get("URL")
+key: str = os.environ.get("KEY")
+
+print(url, key)
