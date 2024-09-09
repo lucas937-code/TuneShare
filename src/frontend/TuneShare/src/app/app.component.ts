696dfa0dc04722786412a5614d1b1e455184baf5
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-09-09 18:44:50 +0200

    f1: MenuBarV1

--- src/frontend/TuneShare/src/app/app.component.ts
+++ src/frontend/TuneShare/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
