8cdf0b1a1de6544c1bfc7774b6be14a6f0c98779
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-09-15 18:30:14 +0200

    added profile page with general design

--- src/frontend/TuneShare/src/app/app.routes.ts
+++ src/frontend/TuneShare/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {PlaylistListComponent} from "./playlistList/playlist-list.component";
 import {HomepageComponent} from "./homepage/homepage.component";
+import {UserProfileComponent} from "./user-profile/user-profile.component";
 
 export const routes: Routes = [
   { path: "library", component: PlaylistListComponent, data : { title: 'Library' } },
   { path: "", component: HomepageComponent },
+  { path: "profile", component: UserProfileComponent },
 ];
---
+++ src/frontend/TuneShare/src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,55 @@
+import {Component, HostListener, Input} from '@angular/core';
+import {PlaylistListComponent} from "../playlistList/playlist-list.component";
+import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {PlaylistComponent} from "../playlist/playlist.component";
+import {PlaylistService} from "../playlist.service";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {waitForAsync} from "@angular/core/testing";
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [
+    PlaylistListComponent,
+    NgForOf,
+    PlaylistComponent,
+    NgIf,
+    NgClass,
+    MatToolbarModule,
+  ],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.scss'
+})
+export class UserProfileComponent {
+  // @Input() user: User = ;    Nutzer injizieren und Variablen anpassen
+  id: number = 0;
+  spotify_id: string | undefined;
+  apple_music_id: string | undefined;
+  date_created: Date = new Date("2000-02-02");
+  username: string = "Max Mustermannn";
+  display_name: string = "Max_2000";
+
+  playlists: any[] = [];
+  isPlaylistsTransformed = false;
+  isFriendsTransformed = false;
+  isShrunk = false;
+
+  constructor(private playlistService: PlaylistService) {}
+
+  ngOnInit() {
+    this.playlists = this.playlistService.getPlaylists();
+  }
+
+  toggleTransformPlaylists() {
+    this.isPlaylistsTransformed = !this.isPlaylistsTransformed;
+  }
+
+  toggleTransformFriends() {
+    this.isFriendsTransformed = !this.isFriendsTransformed;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isShrunk = window.scrollY > 0;
+  }
+}
