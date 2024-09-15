428ddd9456caef6a77cbf47171c328ec77b1700a
KuechenMuesli <kuechenmuesli@gmail.com>
2024-09-15 14:36:43 +0200

    added 3D Parallax effect to playlist covers

--- src/frontend/TuneShare/src/app/playlist/playlist.component.ts
+++ src/frontend/TuneShare/src/app/playlist/playlist.component.ts
@@ -9,4 +9,29 @@ import {Component, Input} from '@angular/core';
 })
 export class PlaylistComponent {
   @Input() playlist: any;
+
+  tilt(event: MouseEvent): void {
+    const img = event.currentTarget as HTMLElement;
+    const imgRect = img.getBoundingClientRect();
+    const imgCenterX = imgRect.left + imgRect.width / 2;
+    const imgCenterY = imgRect.top + imgRect.height / 2;
+
+    const mouseX = event.clientX - imgCenterX;
+    const mouseY = event.clientY - imgCenterY;
+
+    const rotateX = (mouseY / imgRect.height) * 20;
+    const rotateY = (mouseX / imgRect.width) * 20 * (mouseY <= 0 ? 1 : -1);
+
+
+    img.style.setProperty('--rotateX', `${rotateX}deg`);
+    img.style.setProperty('--rotateY', `${rotateY}deg`);
+    img.classList.add('tilted');
+  }
+
+  resetTilt(event: MouseEvent): void {
+    const img = event.currentTarget as HTMLElement;
+    img.classList.remove('tilted');
+    img.style.setProperty('--rotateX', `0deg`);
+    img.style.setProperty('--rotateY', `0deg`);
+  }
 }
