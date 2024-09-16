5d582747828e7d885cb96afa06941cf7b5cf920c
Lucas Hein <lucas.hein.dev@gmail.com>
2024-09-16 14:36:40 +0200

    Toast statt alert (bin jetzt zufrieden)

--- src/frontend/TuneShare/src/app/registration/registration.component.ts
+++ src/frontend/TuneShare/src/app/registration/registration.component.ts
@@ -3,71 +3,72 @@ import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} fr
 import {JsonPipe, NgClass, NgIf, NgOptimizedImage} from "@angular/common";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
 import {Router} from "@angular/router";
+import {NgbToast} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
-    selector: 'app-registration',
-    standalone: true,
-    imports: [
-        ReactiveFormsModule,
-        NgIf,
-        NgClass,
-        JsonPipe,
-        NgOptimizedImage,
-        HttpClientModule
-    ],
-    templateUrl: './registration.component.html',
-    styleUrl: './registration.component.scss'
+  selector: 'app-registration',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+    NgClass,
+    JsonPipe,
+    NgOptimizedImage,
+    HttpClientModule,
+    NgbToast
+  ],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.scss'
 })
 export class RegistrationComponent implements OnInit {
-    readonly window = window;
-    private readonly registrationUrl: string = "http://localhost:8000/register/";
+  readonly window = window;
+  private readonly registrationUrl: string = "http://localhost:8000/register/";
 
-    registerForm!: FormGroup;
+  registerForm!: FormGroup;
 
-    loading: boolean = false;
-    showToast: boolean = false;
+  loading: boolean = false;
+  showToast: boolean = false;
 
-    constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
-    }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
+  }
 
-    ngOnInit(): void {
-        this.registerForm = this.formBuilder.group({
-            email: new FormControl('', [Validators.required, Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
-            password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\w)(?!.* ).{8,}$/)]),
-            repeatPassword: new FormControl('', [Validators.required]),
-            checkbox: new FormControl(false, Validators.requiredTrue)
-        });
-    }
+  ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.pattern(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/)]),
+      password: new FormControl('', [Validators.required, Validators.pattern(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\w)(?!.* ).{8,}$/)]),
+      repeatPassword: new FormControl('', [Validators.required]),
+      checkbox: new FormControl(false, Validators.requiredTrue)
+    });
+  }
 
-    onSubmit(): void {
-        const payload = {email: this.email.value, password: this.password.value};
-        this.loading = true;
-        this.registerForm.disable();
+  onSubmit(): void {
+    const payload = {email: this.email.value, password: this.password.value};
+    this.loading = true;
+    this.registerForm.disable();
 
-        this.http.post(this.registrationUrl, payload).subscribe({
-            error: () => {
-                this.loading = false;
-                this.registerForm.reset();
-                this.registerForm.enable();
-                this.showToast = true;
-                setTimeout(() => this.showToast = false, 5000);
-            },
-            complete: () => {
-                this.loading = false;
-                this.router.navigate(['/']);
-            }
-        });
-    }
+    this.http.post(this.registrationUrl, payload).subscribe({
+      error: () => {
+        this.loading = false;
+        this.registerForm.reset();
+        this.registerForm.enable();
+        this.showToast = true;
+      },
+      complete: () => {
+        this.loading = false;
+        this.router.navigate(['/']);
+      }
+    });
+  }
 
-    get email() {
-        return this.registerForm.controls['email'];
-    }
+  get email() {
+    return this.registerForm.controls['email'];
+  }
 
-    get password() {
-        return this.registerForm.controls['password'];
-    }
+  get password() {
+    return this.registerForm.controls['password'];
+  }
 
-    get repeatPassword() {
-        return this.registerForm.controls['repeatPassword'];
-    }
+  get repeatPassword() {
+    return this.registerForm.controls['repeatPassword'];
+  }
 }
