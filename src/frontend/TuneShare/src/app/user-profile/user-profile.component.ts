e57ccfa486b7c0948a0e472023a8c0f2c6e59145
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-09-17 09:07:18 +0200

    added user and account name to profile

--- src/frontend/TuneShare/src/app/user-profile/user-profile.component.ts
+++ src/frontend/TuneShare/src/app/user-profile/user-profile.component.ts
@@ -6,49 +6,76 @@ import {PlaylistService} from "../playlist.service";
 import {User} from "../types";
 
 @Component({
-  selector: 'app-user-profile',
-  standalone: true,
-  imports: [
-    PlaylistListComponent,
-    NgForOf,
-    PlaylistComponent,
-    NgIf,
-    NgClass,
-  ],
-  templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+    selector: 'app-user-profile',
+    standalone: true,
+    imports: [
+        PlaylistListComponent,
+        NgForOf,
+        PlaylistComponent,
+        NgIf,
+        NgClass,
+    ],
+    templateUrl: './user-profile.component.html',
+    styleUrl: './user-profile.component.scss'
 })
 export class UserProfileComponent {
-  @Input() user: User = {
-    id: 0,
-    spotify_id: undefined,
-    apple_music_id: undefined,
-    date_created: new Date("2000-02-02"),
-    username: "Max Mustermannn",
-    display_name: "Max_2000",
-  }
+    @Input() user: User = {
+        id: 0,
+        spotify_id: undefined,
+        apple_music_id: "test",
+        date_created: new Date("2000-02-02"),
+        username: "Max_2000",
+        display_name: "Max Mustermannn",
+    }
 
-  playlists: any[] = [];
-  isPlaylistsTransformed = false;
-  isFriendsTransformed = false;
-  isShrunk = false;
+    playlists: any[] = [];
+    isPlaylistsTransformed: boolean = false;
+    isFriendsTransformed: boolean = false;
+    isShrunk: boolean = false;
+    isHidden: boolean = false;
+    showCopied: boolean = false;
+    followed: boolean = true; //True = Nutzer wird bereits gefolgt
 
-  constructor(private playlistService: PlaylistService) {}
+    constructor(private playlistService: PlaylistService) {
+    }
 
-  ngOnInit() {
-    this.playlists = this.playlistService.getPlaylists();
-  }
+    ngOnInit() {
+        this.playlists = this.playlistService.getPlaylists();
+    }
 
-  toggleTransformPlaylists() {
-    this.isPlaylistsTransformed = !this.isPlaylistsTransformed;
-  }
+    toggleTransformPlaylists() {
+        this.isPlaylistsTransformed = !this.isPlaylistsTransformed;
+    }
 
-  toggleTransformFriends() {
-    this.isFriendsTransformed = !this.isFriendsTransformed;
-  }
+    toggleTransformFriends() {
+        this.isFriendsTransformed = !this.isFriendsTransformed;
+    }
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    this.isShrunk = window.scrollY > 0;
-  }
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        if (window.scrollY > 0) {
+            this.isShrunk = true;
+            setTimeout(() => {
+                this.isHidden = this.isShrunk
+            }, 200);
+        } else {
+            this.isHidden = false;
+            setTimeout(() => {
+                this.isShrunk = this.isHidden = window.scrollY > 0
+            }, 1);
+        }
+    }
+
+    copyUsername() {
+        navigator.clipboard.writeText("@" + this.user.username).then(() => {
+            setTimeout(() => {
+                this.showCopied = false;
+            }, 800);
+            this.showCopied = true;
+        });
+    }
+
+    follow(){
+        this.followed = !this.followed;
+    }
 }
