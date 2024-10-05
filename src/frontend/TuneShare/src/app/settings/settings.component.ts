d61abe77b6c658e0279b64f847d92f99656b10e8
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-10-06 00:16:43 +0200

    initial-settings-frontend

--- src/frontend/TuneShare/src/app/settings/settings.component.ts
+++ src/frontend/TuneShare/src/app/settings/settings.component.ts
@@ -1,15 +1,26 @@
 import { Component } from '@angular/core';
 import {BACKEND_URL} from "../../main";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
+
+  checkbox: boolean = false;
+
+  saveSettings() {
+    //TODO implement
+  }
+
   linkSpotify() {
     window.location.href = `${BACKEND_URL}service/spotify/?action=login`;
   }
+
 }
