88f8aa49315d48695cfb62254cdea0cc9c3d66f5
Lucas Hein <lucas.hein.dev@gmail.com>
2024-10-09 20:52:35 +0200

    added AuthService to HeaderInterceptor

--- src/frontend/TuneShare/src/app/authentication/shared/headers.interceptor.ts
+++ src/frontend/TuneShare/src/app/authentication/shared/headers.interceptor.ts
@@ -1,9 +1,23 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import {inject} from "@angular/core";
+import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
+import {EMPTY} from "rxjs";
 
 export const headersInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  const accessToken = authService.accessToken;
+
+  if (!accessToken) {
+    router.navigateByUrl('/login').catch(() => console.error('Navigation error occurred'));
+    return EMPTY;
+  }
+
   const authReq = req.clone({
     setHeaders: {
-      Authorization: `Bearer ${localStorage.getItem('token')}`
+      Authorization: `Bearer ${accessToken}`
     }
   })
   return next(authReq);
