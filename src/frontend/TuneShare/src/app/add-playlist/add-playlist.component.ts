4338b4d75be3bc51c87193a0d7c56b6acfc2ad5a
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-10-12 16:23:23 +0200

    alphabetical playlistSort playlistadd

--- src/frontend/TuneShare/src/app/add-playlist/add-playlist.component.ts
+++ src/frontend/TuneShare/src/app/add-playlist/add-playlist.component.ts
@@ -34,7 +34,7 @@ export class AddPlaylistComponent implements OnInit {
       return this.spotifyService.getPlaylists(current_user.id)
     })).subscribe({
       next: playlists => {
-        this.playlistsSpotify = playlists;
+        this.playlistsSpotify = this.sortPlaylistsAlphabetically(playlists);
         this.empty = this.playlistsSpotify.length == 0;
       }
     });
@@ -42,7 +42,7 @@ export class AddPlaylistComponent implements OnInit {
 
     this.appleMusicService.getPlaylists().subscribe(({
       next: playlists => {
-        this.playlistsApplemusic = playlists;
+        this.playlistsApplemusic = this.sortPlaylistsAlphabetically(playlists);
         this.empty = this.playlistsApplemusic.length == 0;
       }
     }));
@@ -50,9 +50,25 @@ export class AddPlaylistComponent implements OnInit {
     this.spotifyActive();
   }
 
+  sortPlaylistsAlphabetically(playlists: Playlist[]) {
+    return playlists.sort((a, b) => {
+      const isNumberOrEmoji = (name: string) => /^[\d\p{Emoji}]/u.test(name.replace(/\s/g, ""));
+      const aIsSpecial = isNumberOrEmoji(a.title);
+      const bIsSpecial = isNumberOrEmoji(b.title);
+      if (aIsSpecial && !bIsSpecial) {
+        return 1;
+      }
+      if (!aIsSpecial && bIsSpecial) {
+        return -1;
+      }
+      return a.title.replace(/\s/g, "").localeCompare(b.title.replace(/\s/g, ""));
+    });
+  }
+
   spotifyActive() {
     this.spotify = true;
     this.empty = this.playlistsSpotify.length === 0;
+    console.log(this.playlistsSpotify[0]);
   }
 
   applemusicActive() {
