aee9f5d6d700b491cc51eacbca9daf1d90736106
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-10-12 12:52:37 +0200

    added stages to export

--- src/frontend/TuneShare/src/app/confirm-export/confirm-export.component.ts
+++ src/frontend/TuneShare/src/app/confirm-export/confirm-export.component.ts
@@ -1,34 +1,50 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {User} from "../types";
-import {NgClass} from "@angular/common";
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-confirm-export',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    NgIf
   ],
   templateUrl: './confirm-export.component.html',
   styleUrl: './confirm-export.component.scss'
 })
-export class ConfirmExportComponent {
+export class ConfirmExportComponent implements AfterViewInit{
 
   @Input() spotify: boolean | undefined;
   @Input() currentPlaylist: any;
+  @Input() id: HTMLDivElement | undefined;
+  stage:number = 1; // 1=confirm; 2=running;3=success;4=failed
 
-  export() {
-    if (this.spotify) {
-      this.exportToSpotify();
-    } else {
-      this.exportToApplemusic();
+  ngAfterViewInit() {
+
+    if (this.id) {
+      this.id.addEventListener('hidden.bs.modal', (event) => {
+        this.stage=1;
+      });
     }
   }
 
-  exportToSpotify() {
-
+  export() {
+    this.stage = 2
+    setTimeout(() => {  //TODO Timeout entfernen und auf Ergebnis warten
+      if (this.spotify) {
+        this.stage = this.exportToSpotify();
+      } else {
+        this.stage = this.exportToApplemusic();
+      }
+    }, 2000);
   }
 
-  exportToApplemusic() {
+  exportToSpotify(): number {
+    let success: boolean = false; //Ergebnis des Exports
+    return success ? 3 : 4;
+  }
 
+  exportToApplemusic():number {
+    let success: boolean = true;  //Ergebnis des Exports
+    return success ? 3 : 4;
   }
 }
