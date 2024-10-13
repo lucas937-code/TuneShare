68e8227c77c7675d7f2aefc52c51fabd5ca355ba
krustus <78026663+badboy00neu@users.noreply.github.com>
2024-10-13 17:25:07 +0200

    Homepage rework

--- src/frontend/TuneShare/src/app/homepage/homepage.component.ts
+++ src/frontend/TuneShare/src/app/homepage/homepage.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {NgClass, NgForOf, NgIf} from "@angular/common";
+import {AsyncPipe, NgClass, NgForOf, NgIf} from "@angular/common";
 import {PlaylistComponent} from "../playlist/playlist.component";
 import {PlaylistRotateComponent} from "../playlist-rotate/playlist-rotate.component";
 import {TuneShareService} from "../tune-share.service";
-import {switchMap} from "rxjs";
+import {Observable, switchMap, tap} from "rxjs";
 import {Playlist, User} from "../types";
 
 @Component({
@@ -14,16 +14,18 @@ import {Playlist, User} from "../types";
     NgForOf,
     PlaylistComponent,
     PlaylistRotateComponent,
-    NgIf
+    NgIf,
+    AsyncPipe
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent implements OnInit {
-  ownPlaylists: any[] = [];
-  followerWithPlaylists: User[] = []
+  ownPlaylists: Playlist[] = [];
+  followerWithPlaylists: {user: User, playlists: Playlist[]}[] = [];
 
-  constructor(private tuneshareService: TuneShareService) {}
+  constructor(private tuneshareService: TuneShareService) {
+  }
 
   ngOnInit(): void {
     this.tuneshareService.getCurrentUser().pipe(switchMap(user => {
@@ -31,6 +33,20 @@ export class HomepageComponent implements OnInit {
     })).subscribe({
       next: playlists => {
         this.ownPlaylists = playlists;
+      }
+    });
+
+    this.tuneshareService.getFollowedUsers().subscribe({
+      next: users => {
+        for (let user of users) {
+          this.tuneshareService.getPlaylistsOfUser(user.id).subscribe({
+            next: playlists => {
+              if (playlists.length > 0) {
+                this.followerWithPlaylists.push({ user, playlists });
+              }
+            }
+          })
+        }
       }
     });
   }
