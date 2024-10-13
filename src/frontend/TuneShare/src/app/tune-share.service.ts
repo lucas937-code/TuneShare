27bda4448d3df797afff8efb5b4e5d18143214ca
KuechenMuesli <=>
2024-10-13 13:07:00 +0200

    added more endpoints

--- src/frontend/TuneShare/src/app/tune-share.service.ts
+++ src/frontend/TuneShare/src/app/tune-share.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import { BACKEND_URL } from "../main";
-import {Playlist, User} from "./types";
-import {Observable} from "rxjs";
+import {Playlist, Track, User} from "./types";
+import {map, Observable, switchMap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -16,17 +16,40 @@ export class TuneShareService {
     return this.http.get<User>(`${BACKEND_URL}api/user/${user_id}/`);
   }
 
-  // TODO
-  // getCurrentUser(): Observable<User> {
-  //
-  // }
+  // get the current user
+  getCurrentUser(): Observable<User> {
+    return this.http.get<User>(`${BACKEND_URL}api/user/current/`);
+  }
 
   // get a list of users the currently logged in user is following
   getFollowedUsers(): Observable<User[]> {
     return this.http.get<User[]>(`${BACKEND_URL}api/user/follows/`);
   }
 
+  // get all TuneShare playlists of a given user
   getPlaylistsOfUser(user_id: number): Observable<Playlist[]> {
     return this.http.get<Playlist[]>(`${BACKEND_URL}api/user/${user_id}/playlists/`);
   }
+
+  // get a specific TuneShare Playlist by id
+  getPlaylist(playlist_id: number): Observable<Playlist> {
+    return this.http.get<Playlist>(`${BACKEND_URL}api/playlist/${playlist_id}/`).pipe(
+      switchMap(playlist =>
+        this.http.get<Track[]>(`${BACKEND_URL}api/playlist/${playlist_id}/tracks`).pipe(
+          map(track_list => {
+            playlist.track_list = track_list;
+            return playlist;
+          })
+        )
+      )
+    );
+  }
+
+  // get a specific Track by id
+  getTrack(track_id: number): Observable<Track> {
+    return this.http.get<Track>(`${BACKEND_URL}api/track/${track_id}/`);
+  }
+
+
+  // searches
 }
