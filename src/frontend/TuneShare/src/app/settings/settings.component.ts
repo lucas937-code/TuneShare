8b4c12f7be928a027a20448a6b85d00101cd433b
KuechenMuesli <=>
2024-10-14 17:57:20 +0200

    implemented settings

--- src/frontend/TuneShare/src/app/settings/settings.component.ts
+++ src/frontend/TuneShare/src/app/settings/settings.component.ts
@@ -6,6 +6,7 @@ import {ActivatedRoute} from "@angular/router";
 import {SpotifyService} from "../spotify.service";
 import {AppleMusicService} from "../apple-music.service";
 import {switchMap} from "rxjs";
+import {TuneShareService} from "../tune-share.service";
 
 @Component({
   selector: 'app-settings',
@@ -19,11 +20,13 @@ import {switchMap} from "rxjs";
 export class SettingsComponent implements OnInit {
 
   constructor(private http: HttpClient, private route: ActivatedRoute, private spotifyService: SpotifyService,
-              private appleMusicService: AppleMusicService) {
+              private appleMusicService: AppleMusicService, private tuneShareService: TuneShareService) {
   }
 
   checkbox: boolean = false;
   code: string | null = null;
+  linkedSpotify: boolean = false;
+  linkedAppleMusic: boolean = false;
 
   ngOnInit() {
     this.route.queryParamMap.subscribe(params => {
@@ -33,17 +36,33 @@ export class SettingsComponent implements OnInit {
           .subscribe()
       }
     });
+
+    this.tuneShareService.linkedServices().subscribe(linkedServices => {
+      this.linkedSpotify = linkedServices.spotify;
+      this.linkedAppleMusic = linkedServices.apple_music;
+    });
   }
 
   linkSpotify() {
-    this.spotifyService.authorizeUser().subscribe();
+    this.spotifyService.authorizeUser().pipe(switchMap(() => {
+      return this.tuneShareService.linkedServices()
+    })).subscribe(linkedServices => {
+      this.linkedSpotify = linkedServices.spotify;
+      this.linkedAppleMusic = linkedServices.apple_music;
+    });
   }
 
   linkAppleMusic() {
     this.appleMusicService.initialize()
       .pipe(switchMap(() => {
         return this.appleMusicService.authorizeUser();
-      })).subscribe({
+      })).pipe(switchMap(() => {
+        return this.tuneShareService.linkedServices()
+    })).subscribe({
+        next: linkedServices => {
+          this.linkedSpotify = linkedServices.spotify;
+          this.linkedAppleMusic = linkedServices.apple_music;
+        },
         error: err => console.error('Failed to authorize user', err)
       });
   }
