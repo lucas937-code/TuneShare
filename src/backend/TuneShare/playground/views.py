69833cc82dfa2060d035bea9382a4ceded3bb018
Lucas Hein <lucas.hein.dev@gmail.com>
2024-10-16 15:14:47 +0200

    removed unused app "playground"

--- src/backend/TuneShare/playground/views.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-from supabase import create_client, Client
-
-supabase_url = settings.SUPABASE_URL
-supabase_key = settings.SUPABASE_KEY
-supabase_client: Client = create_client(supabase_url, supabase_key)
-
-print(supabase_url, supabase_key)
