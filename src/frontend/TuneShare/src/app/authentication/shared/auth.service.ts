bf00645a5f5e858dc4c8d8c6205a1f6076c8c847
Lucas Hein <lucas.hein.dev@gmail.com>
2024-10-16 15:13:55 +0200

    removed unused app "playground"

--- src/frontend/TuneShare/src/app/authentication/shared/auth.service.ts
+++ src/frontend/TuneShare/src/app/authentication/shared/auth.service.ts
@@ -39,7 +39,11 @@ export class AuthService {
   }
 
   get isLoggedIn(): boolean {
-    return !!this.accessToken;
+    if (!this.accessToken)
+      return false;
+    if (this.tokenIsExpired)
+      this.refreshToken().subscribe();
+    return true;
   }
 
   refreshToken(): Observable<string | null> {
